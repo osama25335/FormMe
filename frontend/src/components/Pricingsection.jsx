580faa6e1991d1adfe0cd4a0f36694477355e153
osama25335 <khelifabaghdoucheoussama0@gmail.com>
2025-06-16 18:05:20 +0100

    update4

--- frontend/src/components/Pricingsection.jsx
+++ frontend/src/components/Pricingsection.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import pack from "../objects/package";
 import { PricingCard } from "../ui/pricingcard";
  
 export default function PricingSection(){
@@ -14,15 +13,15 @@ export default function PricingSection(){
                 </div>
                 <div className="flex flex-col lg:flex-row justify-center items-center gap-10">
                     <div className="p-6 shadow hover:shadow-md shadow-primary-color [&>ul]:*:nth-2:text-secondary-lighter w-full md:w-3/4 duration-300">
-                        <PricingCard price={"30$"} tasks={pack} typePlan={"basic plan"} nbrPage={"1"}/>
+                        <PricingCard price={"30$"} typePlan={"basic plan"}/>
                         <Link to="contactus" className=" text-white text-center inline-block w-full py-3 bg-secondary-color hover:bg-secondary-color-hover font-medium max-md:py-1.5 max-md:font-normal duration-200">started</Link>
                     </div>
                     <div className="p-6 shadow hover:shadow-md shadow-primary-color w-full md:w-3/4 duration-300">
-                        <PricingCard price={"50$"} tasks={pack} typePlan={"Standard plan"} nbrPage={"2"}/>
+                        <PricingCard price={"50$"} typePlan={"Standard plan"}/>
                         <Link to="contactus" className=" text-white text-center inline-block w-full py-3 bg-primary-color hover:bg-primary-color-hover font-medium max-md:py-1.5 max-md:font-normal duration-200">started</Link>
                     </div>
                     <div className="p-6 shadow hover:shadow-md shadow-primary-color w-full md:w-3/4 duration-300">
-                        <PricingCard price={"75.55$"} tasks={pack} typePlan={"pro plan"} nbrPage={"3-4"}/> 
+                        <PricingCard price={"75.55$"} typePlan={"pro plan"}/> 
                         <Link to="contactus" className=" text-white text-center inline-block w-full py-3 bg-secondary-color hover:bg-secondary-color-hover font-medium max-md:py-1.5 max-md:font-normal duration-200">started</Link>
                     </div>
                 </div>
--- frontend/src/ui/pricingcard.jsx
+++ frontend/src/ui/pricingcard.jsx
@@ -1,6 +1,5 @@
-import { Check } from "lucide-react";
 
-export const PricingCard = ({typePlan, price, nbrPage, tasks})=>{
+export const PricingCard = ({typePlan, price})=>{
 
     return(
     <>
@@ -8,12 +7,15 @@ export const PricingCard = ({typePlan, price, nbrPage, tasks})=>{
             <span className="font-semibold capitalize">{typePlan}</span>
             <h1 className="pt-4 text-5xl font-bold">{price}</h1>
         </div>
-        <ul className="my-10">{
-                Object.entries(tasks).map(([key, task])=>(
-                    key === "task_1" ? <div className="flex justify-satrt text-secondary-dark items-center space-x-2"><Check className="inline-block text-primary-color" size={24} /> <li className="py-2" key={key}>{nbrPage + task}</li></div> :
-                   <div className="flex justify-satrt items-center text-secondary-dark py-2 space-x-2"><Check className="inline-block text-primary-color" size={24} /> <li className="" key={key}>{task}</li></div> 
-                ))
-        }</ul>
+        <ul className="my-10">
+            <li>page edited</li>
+            <li>Calculation function</li>
+            <li>Drop down menu</li>
+            <li>Text boxes</li>
+            <li>Picture upload</li>
+            <li>File signature</li>
+            <li>Basic function buttons</li>
+        </ul>
     </>
     );
 }
