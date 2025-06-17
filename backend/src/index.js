dce5aa910c269c0f1693256296a37bfc39997849
osama25335 <khelifabaghdoucheoussama0@gmail.com>
2025-06-17 17:17:29 +0100

    update index

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,7 @@ import messageRoutes from "./routes/message.route.js"
 dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,7 +25,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
         app.use(express.static(path.join(__dirname, "../frontend/dist")));
   
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
       res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
